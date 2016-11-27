26e84c5aefa659ea6801760a05de1f972d5e8d66
Netyaga Anastasiya <styusha666@mail.ru>
2016-11-27 23:59:11 +0300

    создание и отладка программы

--- main.cpp
+++ main.cpp
@@ -9,11 +9,28 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc,argv);
+QApplication app(argc,argv);
 
-    QWidget *widget = new QWidget;
+QWidget *widget = new QWidget;
+QPushButton *quitButton = new QPushButton("Quit");
+QRadioButton *radio1 = new QRadioButton("radio 1");
+QRadioButton *radio2 = new QRadioButton("radio 2");
+QRadioButton *radio3 = new QRadioButton("radio 3");
+QVBoxLayout *buttonLayout = new QVBoxLayout;
 
-    widget->show();
+buttonLayout->addWidget(radio1);
+buttonLayout->addWidget(radio2);
+buttonLayout->addWidget(radio3);
+buttonLayout->addWidget(quitButton);
 
-    return app.exec();
+QObject::connect(quitButton,SIGNAL(clicked()),qApp,SLOT(quit()));
+QObject::connect(radio1,SIGNAL(clicked()),qApp,SLOT(setText_2()));
+QObject::connect(radio2,SIGNAL(clicked()),qApp,SLOT(setText_2()));
+QObject::connect(radio3,SIGNAL(clicked()),qApp,SLOT(setText_2()));
+
+widget->setLayout(buttonLayout);
+widget->setWindowFlags(Qt::Window);
+widget->show();
+
+return app.exec();
 }
